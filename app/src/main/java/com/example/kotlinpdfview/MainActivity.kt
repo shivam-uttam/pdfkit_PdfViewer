459d177d50834ca0cf47ccfd375b8acb84fb5f27
shivam-uttam <shivamuttam1010@gmail.com>
2023-05-23 12:06:00 +0530

    pdfkit commit

--- app/src/main/java/com/example/kotlinpdfview/MainActivity.kt
+++ app/src/main/java/com/example/kotlinpdfview/MainActivity.kt
@@ -1,18 +1,21 @@
 package com.example.kotlinpdfview
 
-import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.pspdfkit.configuration.activity.PdfActivityConfiguration
-import com.pspdfkit.ui.PdfActivity
+import androidx.appcompat.app.AppCompatActivity
+import com.github.barteksc.pdfviewer.PDFView
+
+//import com.pspdfkit.configuration.activity.PdfActivityConfiguration
+//import com.pspdfkit.ui.PdfActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val url = Uri.parse("file:///android_asset/hello.pdf")
-        val config = PdfActivityConfiguration.Builder(this).build()
-        PdfActivity.showDocument(this, url, config)
+
+        val pdfView = findViewById<PDFView>(R.id.pdfview)
+        pdfView.fromAsset("hello.pdf")
+            .load()
+
     }
 }
